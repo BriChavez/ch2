3d6fd8f390cce3ed4e1f93e0b0136a58cfd72b59
BriChavez <brianachavez@gmail.com>
2022-04-22 16:44:15 -0700

    cleans up script and checks to see if everything transfred over

--- ch2.sql
+++ ch2.sql
@@ -15,23 +15,22 @@ engine = create_engine(f'{connection_method}://{db_user}:{db_password}@{db_host}
 
 use lego;
 
+
 -- # Select the name and part_num columns from the parts table. Show only names that include the substring 'Hair', and order them with the part_num in descending order
 
-
-
-SELECT p.name, p.part_num
+SELECT name, part_num
 FROM parts p
-WHERE p.name LIKE '%hair%'
+WHERE name LIKE '%hair%'
 GROUP BY part_num DESC;
 
 
 -- # From the sets table, select all the sets that included the word 'Showdown' in the name between 1990 and 2015
 
 
-SELECT s.name, s.year
-FROM sets s
-WHERE s.name like '%showdown%' AND s.year BETWEEN 1990 and 2015
-GROUP BY s.year;
+SELECT name, year
+FROM sets
+WHERE name like '%showdown%' AND year BETWEEN 1990 and 2015
+GROUP BY year;
 
 
 -- # Aliasing the part_categories table as p, show the id and name of the values in that table where the name includes the word 'Bricks'
@@ -41,9 +40,9 @@ FROM part_categories p
 WHERE p.name like '%bricks%';
 
 
--- # Select all the ids from the themes table where the name is in 'Pirates' or 'Star Wars'. 
--- # This is your subquery. Then show the names of all the sets where the theme_id matches an 
--- # id in that subquery.
+
+-- # Select all the ids from the themes table where the name is in 'Pirates' or 'Star Wars'. This is your subquery. Then show the names of all the sets where the theme_id matches an id in that subquery.
+
 
 SELECT s.name, s.theme_id
 FROM sets s
@@ -54,26 +53,8 @@ Where theme_id in
  	AND s.theme_id = t.id);
 
 
+-- # Select the ids of the values in the inventories table that have more than one version (i.e. version > 1). This is your subquery. Then select everything from the inventory_parts table where the inventory_id matches an id in that subquery. Limit the output to 10 rows.
 
--- # Select all the ids from the themes table where the name is in 'Pirates' or 'Star Wars'. 
--- # This is your subquery. Then show the names of all the sets where the theme_id matches an 
--- # id in that subquery.
-
-SELECT s.name, s.theme_id
-FROM sets s
-Where theme_id in
-(SELECT t.id
- 	FROM themes t
- 	WHERE t.name in ('pirates', 'star wars') 
- 	AND s.theme_id = t.id);
-
--- # Select the ids of the values in the inventories table that
--- # have more than one version (i.e. version > 1). This is your
--- # subquery. 
-
--- # Then select everything from the inventory_parts
--- # table where the inventory_id matches an id in that subquery.
--- # Limit the output to 10 rows.
 
 SELECT * 
 FROM inventory_parts 
@@ -82,18 +63,17 @@ WHERE inventory_id IN
      FROM inventories 
      WHERE version > 1);
 
--- # Aliasing the sets table as 's', select the year and name 
--- # of the values in that table that include the substring 
--- # 'Batman'. Concatenate three exclamation marks to the end 
--- # of each name, and make all the names upper case.
 
-SELECT year, CONCAT(UPPER(s.name), '!!!') as names_cap 
+-- # Aliasing the sets table as 's', select the year and name of the values in that table that include the substring 'Batman'. Concatenate three exclamation marks to the end of each name, and make all the names upper case.
+
+
+SELECT s.year, CONCAT(UPPER(s.name), '!!!') as names_cap 
 FROM sets s
-WHERE name like '%batman%';
+WHERE s.name like '%batman%';
 
 
--- # For every quantity value greater than 1 in the inventory_parts table, double the value 
--- # of the quantity. Limit your output to 20 rows, and order it by quantity.
+-- # For every quantity value greater than 1 in the inventory_parts table, double the value of the quantity. Limit your output to 20 rows, and order it by quantity.
+
 
 SELECT
     quantity,
@@ -103,43 +83,37 @@ WHERE quantity > 1
 ORDER BY quantity DESC LIMIT 20;
 
 
--- # For every quantity value greater than 1 in the inventory_parts table, double the value 
--- # of the quantity. Limit your output to 20 rows, and order it by quantity.
-
-SELECT
-    quantity,
-    quantity * 2 AS doubled
-FROM inventory_parts 
-WHERE quantity > 1
-ORDER BY quantity DESC LIMIT 20;
+--  Select the name and year column. Construct an additional column in your SELECT clause using the MySQL MAKEDATE() function. This column's value should be a date corresponding to January 1st of the year. 
 
 
--- # For every quantity value greater than 1 in the inventory_parts table, double the value 
--- # of the quantity. Limit your output to 20 rows, and order it by quantity.
+SELECT s.name, s.year, MAKEDATE(s.year,1) as the_first
+FROM sets s;
 
-SELECT
-    quantity,
-    quantity * 2 AS doubled
-FROM inventory_parts 
-WHERE quantity > 1
-ORDER BY quantity DESC LIMIT 20;
+-- Count the total number of transparent colors in the colors table
+
+-- (Note: is_trans is a binary value, and you want to show how many rows there are where it equals 1).
 
 
--- # For every quantity value greater than 1 in the inventory_parts table, double the value 
--- # of the quantity. Limit your output to 20 rows, and order it by quantity.
+SELECT COUNT(distinct name)
+FROM colors
+WHERE name like '%trans%';
 
-SELECT
-    quantity,
-    quantity * 2 AS doubled
-FROM inventory_parts 
-WHERE quantity > 1
-ORDER BY quantity DESC LIMIT 20;
+
+-- Show the sum of all the parts for all the sets that came out since the year 2000.
+
+
+SELECT SUM(num_parts) as sum_parts
+FROM sets
+WHERE sets.year > 2000;
+
 
 -- # Show the average number of parts for sets that came out since the year 2000.
+
 
 SELECT AVG(num_parts) as avg_parts 
 FROM sets
 WHERE year >1999;
+
 
 -- # Using GROUP BY, show the average number of parts for each theme_id in sets.
 
@@ -147,9 +121,9 @@ SELECT theme_id, AVG(num_parts) avg_parts
 FROM sets
 GROUP BY theme_id
 
--- # Using a free-form join (where the JOIN type isn't specified, equivalent to an inner join), join the parts and 
--- # part_categories tables. Filter for values where the part_categories id matches the parts 
--- # partcatid, and where the name of the part contains the word 'Werewolf'.
+
+-- # Using a free-form join (where the JOIN type isn't specified, equivalent to an inner join), join the parts and part_categories tables. Filter for values where the part_categories id matches the parts partcatid, and where the name of the part contains the word 'Werewolf'.
+
 
 SELECT part_categories.id, parts.part_cat_id, parts.name
 FROM parts
@@ -157,8 +131,10 @@ JOIN part_categories
 ON part_categories.id = parts.part_cat_id
 WHERE parts.name like '%werewolf%';
 
+
 -- # Repeat the query above, but this time write it explicitly using INNER JOIN, and alias parts as 'p' 
 -- # and part_categories as 'pc'.
+
 
 SELECT pc.id, p.part_cat_id, p.name
 FROM parts p
